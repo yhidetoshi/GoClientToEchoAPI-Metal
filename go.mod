23f05e73084267a1ab55ccac64fae4bcb7b642cd
yhidetoshi <hyajima.1014@gmail.com>
2019-09-23 07:35:52 +0900

    commit first all

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/yhidetoshi/GoClientToEchoAPI-MetalToMackerel
+
+go 1.12
+
+require (
+	github.com/aws/aws-lambda-go v1.13.2
+	github.com/hashicorp/go-version v1.2.0 // indirect
+	github.com/mackerelio/mackerel-client-go v0.8.0
+	github.com/mitchellh/gox v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/mackerelio/mackerel-client-go"
+)
+
+var (
+	// url = "http://localhost:1323/metal"
+	url    = os.Getenv("APIURL")
+	mkrKey = os.Getenv("MKRKEY")
+	client = mackerel.NewClient(mkrKey)
+)
+
+const (
+	serviceName = "Metal"
+	timezone    = "Asia/Tokyo"
+	offset      = 9 * 60 * 60
+)
+
+type Metal struct {
+	Date     time.Time `json:"time"`
+	GoldInfo GoldInfo  `json:"goldInfo"`
+	Platinum Platinum  `json:"platinum"`
+}
+
+type GoldInfo struct {
+	RetailTax   int `json:"retailTax"`
+	PurchaseTax int `json:"purchaseTax"`
+}
+
+type Platinum struct {
+	RetailTax   int `json:"retailTax"`
+	PurchaseTax int `json:"purchaseTax"`
+}
+
+func main() {
+	lambda.Start(Handler)
+}
+
+// func main() {
+func Handler() {
+	req, _ := http.NewRequest("GET", url, nil)
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	metal := &Metal{}
+
+	err = json.NewDecoder(resp.Body).Decode(metal)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	jst := time.FixedZone(timezone, offset)
+	nowTime := time.Now().In(jst)
+
+	mkrErr := PostValuesToMackerel(metal.GoldInfo.RetailTax, metal.GoldInfo.PurchaseTax, metal.Platinum.RetailTax, metal.Platinum.PurchaseTax, nowTime)
+	if mkrErr != nil {
+		fmt.Println(mkrErr)
+	}
+}
+
+// PostValuesToMackerel Post Metrics to Mackerel
+func PostValuesToMackerel(goldRetailTax int, goldPurchaseTax int, platinumRetailTax int, platinumPurchaseTax int, nowTime time.Time) error {
+	// Post Gold metrics
+	errGold := client.PostServiceMetricValues(serviceName, []*mackerel.MetricValue{
+		&mackerel.MetricValue{
+			Name:  "Gold.retail_tax",
+			Time:  nowTime.Unix(),
+			Value: goldRetailTax,
+		},
+		{
+			Name:  "Gold.purchase_tax",
+			Time:  nowTime.Unix(),
+			Value: goldPurchaseTax,
+		},
+	})
+	if errGold != nil {
+		fmt.Println(errGold)
+	}
+
+	// Post Platinum metrics
+	errPlatinum := client.PostServiceMetricValues(serviceName, []*mackerel.MetricValue{
+		&mackerel.MetricValue{
+			Name:  "Platinum.retail_tax",
+			Time:  nowTime.Unix(),
+			Value: platinumRetailTax,
+		},
+		{
+			Name:  "Platinum.purchase_tax",
+			Time:  nowTime.Unix(),
+			Value: platinumPurchaseTax,
+		},
+	})
+	if errPlatinum != nil {
+		fmt.Println(errPlatinum)
+	}
+
+	return nil
+}
